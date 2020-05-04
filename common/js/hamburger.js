fc8469ea3e9704f163fb6adffae6e53488c04c1d
ngsw877 <otsda1055@yahoo.co.jp>
2020-05-04 18:46:30 +0900

    ハンバーメニュークリック後のモーダル実装('#2')

--- common/js/hamburger.js
+++ common/js/hamburger.js
@@ -1,9 +1,21 @@
+//ハンバーガーメニュー
+
 $(function(){
 
   var $btn = $('.nav-button');
+  var $menu = $('.menu-area');
   // console.log($btn);
+  // console.log($menu);
+
   $btn.on('click', function() {
-    $(this).toggleClass('active');
+
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $('.menu-area').slideUp();
+    } else {
+      $(this).addClass('active');
+      $('.menu-area').slideDown();
+    }
     return false;
   });
 });
