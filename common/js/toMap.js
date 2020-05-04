9be2bd6217dd88ee85dd05b288ab77ed17fa8026
ngsw877 <otsda1055@yahoo.co.jp>
2020-05-04 20:05:37 +0900

    アクセスマップへの、スクロール移動距離の調整処理を追加('#2')

--- common/js/toMap.js
+++ common/js/toMap.js
@@ -1,8 +1,31 @@
+// アクセスマップへスクロール移動（スムーススクロール）
+
 $(function(){
   $('a[href="#access"]').click(function(){
     var speed = 500;
     var target = $(this).attr("href");
     var position = $(target).offset().top;
+
+    //~~ どこまでスクロールするのか、位置を調整 ~~//
+
+      //ヘッダーからジャンプする場合
+    if($(this).hasClass("to-map1")){
+      position -= $(".header-top").innerHeight() + $(".menu-area").innerHeight();
+      //ハンバーガーメニューもしくは、ヘッダーからジャンプする場合
+    } else {
+        var winW = $(window).width();
+        var divW = 900; //900px以下になると、ハンバーガーメニュー付ヘッダーに切り替わるため
+        if(winW <= divW){
+          //900px以下の場合
+          position -= $(".header-top").innerHeight();
+        } else {
+          //900pxより大きい場合
+          position -= $(".sticky-header").innerHeight();
+        }
+    }
+
+    // console.log(this);
+    // console.log(position);
     $("html, body").animate({
       scrollTop: position
     }, speed);
